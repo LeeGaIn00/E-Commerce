cc7c99a9bec806eea58c39df47c82acbf97edb8c
LeeGaIn00 <rkdls7815@naver.com>
2023-01-06 12:36:11 +0900

    front setting2

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Routes} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-      </Routes>
-    </BrowserRouter>
-   
+    <Routes>
+    </Routes>
   );
 }
 
